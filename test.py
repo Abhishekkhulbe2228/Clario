88340486dcfa79b4ee5220cca0c4ad30f01442c2
Abhishekkhulbe2228 <your-email@example.com>
2026-09-07 16:54:32 +0530

    Added the ingestion pipeline(Load + chunk)

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from  app.core.config import get_settings
+from  app.services.ingestion import load_file, chunk_documents
+from pathlib import Path
 
-settings = get_settings()
+docs = load_file(Path("data/sample_kb/company_hr_handbook.md"))
+chunked_docs = chunk_documents(docs)
 
-print(f"Tavily AI api: {settings.tavily_api_key}")
+print(len(chunked_docs))
